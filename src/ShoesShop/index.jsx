93a0e5a105ce5bc59c42ab52e0187c8f6ccf0eba
HungTran98 <hungtranitvn@gmail.com>
2023-04-19 03:45:01 +0700

    fix index.jsx

--- src/ShoesShop/index.jsx
+++ src/ShoesShop/index.jsx
@@ -2,14 +2,10 @@ import React, { Component } from "react";
 import ProductList from "./ProductList";
 
 export default class ShoesShop extends Component {
-  state = {
-    productDentail: {},
-  };
-  products = [];
   render() {
     return (
       <div>
-        <ProductList productsData={this.products} setStateModal={this.state} />
+        <ProductList />
       </div>
     );
   }
